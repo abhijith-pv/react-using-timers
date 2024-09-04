83178408ff9dae4daa2ab472b70814ff0c37c105
Abhijith P V <abhijithpv9087@gmail.com>
2024-09-04 08:56:22 +0530

    user can enter name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import {useState} from "react";
+
+
 export default function Player() {
+
+  const [enterdPlayerName, setEnteredPlayerName]= useState('');
+  const [submitted, setSumbitted]= useState('false');
+
+  function handleChange(event){
+    setEnteredPlayerName(event.target.value);
+  }
+  function handleClick(){
+    setSumbitted(true);
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome { submitted? enterdPlayerName:'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enterdPlayerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
