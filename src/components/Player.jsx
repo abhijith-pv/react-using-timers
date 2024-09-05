0a6d2e98565cda5117de396ddb4f5211b44d33f7
Abhijith P V <abhijithpv9087@gmail.com>
2024-09-05 22:13:36 +0530

    user can stop and start the timer

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,20 +1,20 @@
-import {useState , useRef} from "react";
+import { useState, useRef } from "react";
 
 
 export default function Player() {
-  
-  const playerName= useRef();
-  const [enteredPlayerName, setEnteredPlayerName]= useState(null);
 
-  
-  function handleClick(){
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+
+  function handleClick() {
     setEnteredPlayerName(playerName.current.value);
   }
   return (
     <section id="player">
-      <h2>Welcome {  enteredPlayerName ?? 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input  ref={playerName} type="text"  />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
